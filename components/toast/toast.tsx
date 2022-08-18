754b1790336bda702554cf3d09328d49b199297f
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-08-18 18:50:43 +0100

    notifcations fixed

--- components/toast/toast.tsx
+++ components/toast/toast.tsx
@@ -1,12 +1,29 @@
 import styles from "./toast.module.css";
 import {Itoast} from "./toastList";
+import { IoAlertCircleOutline, IoWarningOutline, IoInformationCircleOutline, IoCheckmarkCircleOutline } from "react-icons/io5";
 
+const icon = (type: "success" | "error" | "warning" | "info" | "primary") => {
+     switch (type) {
+          case "success":
+               return <IoCheckmarkCircleOutline size={20}  className={styles.icon} />;
+          case "error":
+               return <IoAlertCircleOutline size={20}  className={styles.icon} />;
+          case "warning": 
+               return <IoWarningOutline size={20}  className={styles.icon} />;
+          case "info": 
+               return <IoInformationCircleOutline size={20}  className={styles.icon} />;
+          case "primary":
+               return "";
+}
+}
 export function Toast({data}: {data: Itoast}) {
+
      return (
           <div className={`${styles.container} ${styles.notification} ${styles[data.type]} `}>
-               <div>
+               <div className={`${styles.body}`}>
                     {/* <p>{data.title}</p> */}
-                    <p>{data.message}</p>
+                    {icon(data.type)}
+                    <p> {data.message}</p>
                </div>
           </div>
      )
