d68a77cc15984aa11b29d7fb0e41e0540f055777
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-08-20 19:09:31 +0100

    fixed pwa error

--- next.config.js
+++ next.config.js
@@ -1,12 +1,5 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-};
+const withPWA = require("next-pwa");
 
-module.exports = nextConfig;
-
-const withPWA  = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
