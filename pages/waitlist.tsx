99dbfd28c921517159a9767e22c2d00b459b9edc
fortune710 <fortune0208@yahoo.com>
2022-08-22 23:00:04 +0100

    basic set up for waitlist page

---
+++ pages/waitlist.tsx
@@ -0,0 +1,11 @@
+import { NextPage } from "next";
+
+const WaitlistPage: NextPage = () => {
+    return(
+        <div>
+            <h1>Waitlist Page</h1>
+        </div>
+    )
+}
+
+export default WaitlistPage;
