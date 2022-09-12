1e48290cc31304b24b954193b79555ac62c54335
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-09-12 07:45:44 +0100

    added enums files

---
+++ services/enums/post.ts
@@ -0,0 +1,35 @@
+export const post = [
+     {
+          _id: "dfddf233233",
+          author: '456ydhhj',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget congue ornare consequat eget enim. Risus fringilla posuere amet sagittis habitant nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget congue ornare consequat eget enim. Risus fringilla posuere amet sagittis habitant nulla.",
+          image: "/assets/images/post.png",
+          createdAt: "Mon Sep 12 2022 07:34:34 GMT+0100 (West Africa Standard Time)",
+          comments:[
+               {
+                    _id: "com4676",
+                    author: "456ydhh999j",
+                    content: "You are doing a great job!",
+               }
+          ]
+     },
+     {
+          _id: "dfddf299933",
+          author: '456ydhh999j',
+          content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget congue ornare consequat eget enim. Risus fringilla posuere amet sagittis habitant nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Eget congue ornare consequat eget enim. Risus fringilla posuere amet sagittis habitant nulla.",
+          image: "",
+          createdAt: "Mon Sep 11 2022 07:34:34 GMT+0100 (West Africa Standard Time)",
+          comments:[
+               {
+                    _id: "com46h900hg76",
+                    author: "12d999hj",
+                    content: "You are doing a great job!",
+               },
+               {
+                    _id: "com46h66hg76",
+                    author: "456ydhhj",
+                    content: "You are doing a fantastic job!",
+               },
+          ]
+     }
+] as const;
---
+++ services/enums/profiles.ts
@@ -0,0 +1,21 @@
+export const profiles = [
+     {
+          _id: "456ydhhj",
+          name: "John Faruk",
+          position: "Google",
+          imageUrl: "/assets/images/user.png",
+     },
+     {
+          _id: "456ydhh999j",
+          name: "Ella Belle",
+          position: "Microsoft",
+          imageUrl: "/assets/images/userfemale.png",
+     },
+     {
+          _id: "12d999hj",
+          name: "John Taiwo",
+          position: "Flutterwave",
+          imageUrl: "/assets/images/user2.png",
+     }
+
+] as const;
