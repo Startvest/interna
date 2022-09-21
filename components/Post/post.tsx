23d0961df1bee783f95577ddf01abb502d144213
fortune710 <fortune0208@yahoo.com>
2022-09-21 01:04:22 +0100

    completed post component

--- components/Post/post.tsx
+++ components/Post/post.tsx
@@ -1,28 +1,31 @@
 import { Avatar } from '../Avatar';
+import { useState } from 'react';
 import DisplayDate from '../DisplayDate';
 import { MdFavoriteBorder, MdFavorite, MdShare, MdSend, MdComment } from 'react-icons/md';
 import styles from './post.module.scss';
+import { useRouter } from 'next/router';
 
-interface Comment {
-    _id: string,
-    author: string,
-    content: string,
-}
-
-interface Post extends Comment {
-    image: string,
-    createdAt: string,
-    comments: Comment[]
-}
 
 type PostProps = {
     image: string,
     createdAt: string,
-    content: string
+    content: string,
+    author: string,
+    _id: string
 }
 
 export const Post: React.FC<PostProps> = (props) => {
-    const {  content, createdAt, image } = props;
+    const { _id, author, content, createdAt, image } = props;
+    const [showMore, setShowText] = useState(false);
+    const router = useRouter()
+
+    const sharePost = () => {
+        navigator.share({
+            title: `Check out this post by ${author}`,
+            text: content,
+            url: `https://getinterna.com/post/${_id}`
+        })
+    }
     
     return(
         <div className={styles.post}>
@@ -39,18 +42,33 @@ export const Post: React.FC<PostProps> = (props) => {
             </div>
 
             <div className={styles.postContent}>
-                { content }
+                { showMore ? content : content.substring(0, 65)+"..." }
+                <span className={styles.showMoreBtn} onClick={() => setShowText(!showMore)}>
+                    { showMore ? 'see less' : 'see more' }
+                </span>
             </div>
 
-            <div className={styles.imageHolder}>
+            <div className={styles.imageHolder} onClick={() => router.push(`/post/${_id}`)}>
                 <img src={image} alt={`a post`}/>
             </div>
 
             <div className={styles.postActions}>
-                <MdFavoriteBorder size={25}/>
-                <MdComment size={25}/>
-                <MdSend size={25}/>
-                <MdShare size={25}/>
+                <span>
+                    <MdFavoriteBorder size={25}/>
+                    <p>10K</p>
+                </span>
+                <span>
+                    <MdComment size={25}/>
+                    <p>2K</p>
+                </span>
+                <span>
+                    <MdSend size={25}/>
+                    <p>Send</p>
+                </span>
+                <span onClick={sharePost}>
+                    <MdShare size={25}/>
+                    <p>Share</p>
+                </span>
             </div>
         </div>
     )
