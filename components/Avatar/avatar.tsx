8f65a3d97163016cb57f8afe8b305e39ae349e85
fortune710 <fortune0208@yahoo.com>
2022-09-22 13:32:43 +0100

    modified avatar component to have different size

--- components/Avatar/avatar.tsx
+++ components/Avatar/avatar.tsx
@@ -3,12 +3,13 @@ import styles from './avatar.module.scss';
 type AvatarProps = {
     src: string
     alt?: string
+    size?: 'small'|'large'
 }
 
-export const Avatar: React.FC<AvatarProps> = ({ src, alt }) => {
+export const Avatar: React.FC<AvatarProps> = ({ size, src, alt }) => {
     return(
-        <div className={styles.avatar}>
-            <img src={src} alt={alt?alt:'profile-avatar'} width={'50px'}/>
-        </div>
+        <img 
+        className={`${styles.avatar} ${size === 'small'? styles.small : styles.large}`} 
+        src={src} alt={alt?alt:'profile-avatar'}/>
     )
 }
