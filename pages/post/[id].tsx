5445c5fd930bcc87060835c7f41fc32ffa472738
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-09-22 22:03:23 +0100

    fixed vercel error

--- pages/post/[id].tsx
+++ pages/post/[id].tsx
@@ -47,7 +47,7 @@ export const PostDetail: React.FC = () => {
         <MdDragIndicator size={30}/>
        </Toolbar>
         <div className={styles.userInfo}>
-            <Avatar src='/assets/user.png' width="80px"/>
+            <Avatar src='/assets/user.png' size="small"/>
 
             <div>
                 <span>
