3fdd764fa4dbc27b71155817995b1f0ee87aa284
fortune710 <fortune0208@yahoo.com>
2022-09-22 10:12:34 +0100

    set dynamic icons for floating navbar

--- components/FloatingNavbar/navbar.tsx
+++ components/FloatingNavbar/navbar.tsx
@@ -1,6 +1,11 @@
 import styles from './navbar.module.css';
 import {useRouter} from 'next/router';
 import {
+     IoAlbums,
+     IoSearch,
+     IoMailUnread,
+     IoNotifications,
+     IoSettings,
      IoAlbumsOutline,
      IoSearchOutline,
      IoNotificationsOutline,
@@ -8,20 +13,29 @@ import {
      IoMailUnreadOutline,
      IoSettingsOutline
    } from 'react-icons/io5';
+import Link from 'next/link';
 
-interface pathNames{
-     path: '/feed' | '/search' |'/notifications' | '/messages' | '/settings'
-}
+type pathNames = '/feed' | '/search' |'/notifications' | '/messages' | '/settings'
+
 export function NavBar(){
      const router = useRouter();
      // console.log(router.pathname);
+
+     const path:pathNames = router.pathname as pathNames
+
+     let albumsIcon = path === '/feed' ? <IoAlbums size={30} className={styles.icon}/> : <IoAlbumsOutline size={30} className={styles.icon}/>
+     let searchIcon = path === '/search' ? <IoSearch size={30} className={styles.icon}/> : <IoSearchOutline size={30} className={styles.icon}/>
+     let notificationsIcon = path === '/notifications' ? <IoNotifications size={30} className={styles.icon}/> : <IoNotificationsOutline size={30} className={styles.icon}/>
+     let mailIcon = path === '/messages' ? <IoMailUnread size={30} className={styles.icon}/> : <IoMailUnreadOutline size={30} className={styles.icon}/>
+     let settingsIcon = path === '/settings' ? <IoSettings size={30} className={styles.icon}/> : <IoSettingsOutline size={30} className={styles.icon}/>
+     
      return(
           <div className={styles.container}>
-               <IoAlbumsOutline size={30} className={styles.icon} />
-               <IoSearchOutline size={30} className={styles.icon} />
-               <IoNotificationsOutline size={30} className={styles.icon} />
-               <IoMailUnreadOutline size={30} className={styles.icon} />
-               <IoSettingsOutline size={30} className={styles.icon} />
+               <Link href={'/feed'}>{ albumsIcon }</Link>
+               <Link href={'/search'}>{ searchIcon }</Link>
+               <Link href={'/notifications'}>{ notificationsIcon }</Link>
+               <Link href={'/messages'}>{ mailIcon }</Link>
+               <Link href={'/settings'}>{ settingsIcon }</Link>
           </div>
      )
 }
