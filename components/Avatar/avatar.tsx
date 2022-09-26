b038252bc12340ecc0b184a056f9bc9ae4d87ace
fortune710 <fortune0208@yahoo.com>
2022-09-26 14:07:47 +0100

    avatar has 3 sizes via props; small, medium &large

--- components/Avatar/avatar.tsx
+++ components/Avatar/avatar.tsx
@@ -3,13 +3,13 @@ import styles from './avatar.module.scss';
 type AvatarProps = {
     src: string
     alt?: string
-    size?: 'small'|'large'
+    size?: 'small'|'large'|'medium'
 }
 
 export const Avatar: React.FC<AvatarProps> = ({ size, src, alt }) => {
     return(
         <img 
-        className={`${styles.avatar} ${size === 'small'? styles.small : styles.large}`} 
+        className={`${styles.avatar} ${size === 'small'? styles.small : size === 'large' ? styles.large : styles.medium}`} 
         src={src} alt={alt?alt:'profile-avatar'}/>
     )
 }
