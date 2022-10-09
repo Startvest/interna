da9bf66be86115ec45d1b28b9951bb8fdd0b7ec0
fortune710 <fortune0208@yahoo.com>
2022-10-09 18:28:25 +0100

    search page

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -1,10 +1,26 @@
 import { NavBar } from "../../components/FloatingNavbar";
 import { Searchbar } from "../../components/Searchbar"
 
+
+import styles from './search.module.scss';
 const SearchPage: React.FC = () => {
     return(
-    <main>
+    <main className={styles.searchPage}>
         <Searchbar placeholder="Search for anything"/>
+        
+        <div className={`${styles.container} ${styles.filterBox}`}>
+            <div className={styles.buttonHolder}>
+                <span>
+                    Interns
+                </span>
+                <span>
+                    Companies
+                </span>
+            </div>
+        </div>
+        
+        
+        
         <NavBar/>
     </main>
     )
