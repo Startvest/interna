b0f64ea63b4779a2d1f5c7eafb04a6ef7ab81bf0
Tamunokorite Briggs <obriggs03@gmail.com>
2022-10-11 00:23:44 +0100

    Adding date created to waitlist member model

--- pages/api/db/waitlist.db.ts
+++ pages/api/db/waitlist.db.ts
@@ -1,11 +1,10 @@
 const uuid = require('uuid');
 
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const url: string = process.env.DB_URL || "mongodb://127.0.0.1:27017/";
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASS}@internadb.1a9jv1q.mongodb.net/?retryWrites=true`;
+const uri: string = `${process.env.DB_URL}`;
 const db_name: string = process.env.DB_NAME || "interna_db";
 
-const waitlistDb : any = {};
+const waitlistDb: any = {};
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
@@ -14,23 +13,26 @@ class WaitlistMember {
   public name: string;
   public email: string;
   public position: {type: "string", company_name: "string"};
+  public created: string
 
-  public constructor(waitlist_id: string, name: string, email: string, position: {type: "string", company_name: "string"}) {
+  public constructor(waitlist_id: string, name: string, email: string, position: {type: "string", company_name: "string"}, created: string) {
       this.waitlist_id = waitlist_id;
       this.name = name;
       this.email = email;
       this.position = position;
+      this.created = created
   }
 }
 
 waitlistDb.createWaitlistMember = async (data: {name: string, email: string, position: {type: "string", company_name: "string"}} ) => {
     try {
       await client.connect();
-      let member: {waitlist_id: string, name: string, email: string, position: {type: "string", company_name: "string"}} = {
+      let member: {waitlist_id: string, name: string, email: string, position: {type: "string", company_name: "string"}, created: string} = {
         waitlist_id: uuid.v4(),
         name: data.name,
         email: data.email,
-        position: data.position
+        position: data.position,
+        created: Date(Date.now()).toString()
       };
 
       let email_check = await client.db(db_name).collection("waitlist").findOne({email: member.email});
@@ -64,7 +66,7 @@ waitlistDb.getMembers = async () => {
       console.log(results);
 
       for (let result of results) {
-        response.push(new WaitlistMember(result.waitlist_id, result.name, result.email, result.position));
+        response.push(new WaitlistMember(result.waitlist_id, result.name, result.email, result.position, result.created));
       }
 
       client.close();
