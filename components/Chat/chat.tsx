484ed1b226a79a12438d23982930db1e9c138e6d
fortune710 <fortune0208@yahoo.com>
2022-10-11 18:20:25 +0100

    Modified the chat page

--- components/Chat/chat.tsx
+++ components/Chat/chat.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { IoEllipse } from "react-icons/io5";
 import { Avatar } from "../Avatar";
 import DisplayDate from "../DisplayDate";
@@ -8,20 +9,26 @@ type ChatProps = {
     dateCreated: any,
     profileURL: string,
     sender: string,
-    message: string
+    message: string,
+    opened: boolean
 }
 export const Chat: React.FC<ChatProps> = (props) => {
+    const [opened, setOpened] = useState(props.opened);
+
     return(
-        <div className={styles.chat}>
+        <div onClick={() => setOpened(true)} className={styles.chat}>
             <Avatar src={props.profileURL} size="small" className={styles.avatar} />
-            <div>
+            <div className={styles.details}>
                 <span>
                     <h3>{props.sender}</h3>
                     <IoEllipse size={5}/>
                     <DisplayDate date={props.dateCreated} show={'ago'}/>
                 </span>
-                    <p>{props.message.substring(0, 45)+"..."}</p>
+                <p>{props.message.substring(0, 28)+"..."}</p>
             </div>
+            {
+                !opened ? <span className={styles.openedIndicator}/> : null
+            }
         </div>
     )
 }
