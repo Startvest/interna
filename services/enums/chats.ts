66996626a5308e484fa005f077206247997151ec
fortune710 <fortune0208@yahoo.com>
2022-10-11 14:53:40 +0100

    added more test data

--- services/enums/chats.ts
+++ services/enums/chats.ts
@@ -5,20 +5,23 @@ export const Chats = [
         message: 'Hello!',
         dateCreated: new Date().toUTCString(),
         id: 1,
+        opened: false
     },
     {
-        profileURL: 'assets/images/Fortune.jpg',
-        sender: 'Fortune Alebiosu',
-        message: 'Hello!',
+        profileURL: 'assets/users/mubarak-showole.jpg',
+        sender: 'Mubarak Showole',
+        message: 'Hi John, nice connecting with you',
         dateCreated: new Date().toUTCString(),
         id: 2,
+        opened: true
     },
     {
-        profileURL: 'assets/images/Fortune.jpg',
-        sender: 'Fortune Alebiosu',
-        message: 'Hello!',
+        profileURL: 'assets/users/favour-adeyemi-jpg',
+        sender: 'Favour Adeyemi',
+        message: 'What programming language do you work with?',
         dateCreated: new Date().toUTCString(),
         id: 3,
+        opened: true
     }
 
 ]
--- services/enums/post.ts
+++ services/enums/post.ts
@@ -1,5 +1,48 @@
 export const post = [
      {
+          _id: "yds62Ds8dx ",
+          author: {
+               id: "Fjd65Bs8pwq6",
+               name: "Elizabeth Akpan",
+               position: "Frontend Intern at O-Pay",
+               image: "/assets/users/elizabeth-akpan.jpg"
+          },
+          content: `
+          Excited to announce that I have been accepted as a Frontend Developer Intern at O-Pay!🎉🎉
+
+          I'm thrilled and I can't to see what the future holds.🚀
+          `,
+          createdAt: "Sat Oct 8 2022 07:34:34 GMT+0100 (West Africa Standard Time)",
+          comments: [
+               {
+                    _id: "s6y7ty2r23e2",
+                    author: {
+                         id: "h64yT2dsD",
+                         name: "Favour Adeyemi",
+                         position: "Frontend Developer Intern",
+                         image: "/assets/users/favour-adeyemi.jpg",
+                    },
+                    content: "Congratulations Elizabeth!!",
+                    createdAt: "Mon Oct 10 2022 14:40:34 GMT+0100 (West Africa Standard Time)",
+                    likes: ["Fjd65Bs8pwq6"]
+               },
+               {
+                    _id: "ywvi8vs52t2",
+                    author: {
+                         id: "wq83g56q21",
+                         name: "O-Pay Nigeria",
+                         position: "Payment Service",
+                         image: "/assets/companies/o-pay.webp",
+                    },
+                    content: "Welcome to the team, Elizabeth",
+                    createdAt: "Tue Oct 11 2022 14:40:34 GMT+0100 (West Africa Standard Time)",
+                    likes: ["Fjd65Bs8pwq6"]
+               }
+
+          ],
+          likes: []
+     },
+     {
           _id: "dfddf233233",
           author: { 
                id: "456ydhh999j",
@@ -142,5 +185,48 @@ export const post = [
                },
           ],
           likes : ["456ydhh999j"]
-     }
+     },
+     {
+          _id: "jH67dk9SFkdgdv",
+          author: {
+               id: "dgv52u87dgcx",
+               name: "Fortune Alebiosu",
+               position: "Intern at Microsoft",
+               image: "/assets/images/Fortune.jpg"
+          },
+          content: `
+          THINGS I WISH I KNEW BEFORE BECOMING A DEVELOPER \n\n
+
+          1. Programming Is All About Having The Right Resources And Selecting The Best Tool For The Job \n
+
+          2. Imposter Syndrome Is Normal At Every Stage.
+
+          3. Cheating is Perfectly Acceptable. Like most programmers, I frequently turn to Google to find solutions.
+
+          4. There is A Significant Difference Between A Capital And a Lowercase Letter. It took me a while to get used to 
+          noticing minute variations between seemingly identical symbols, 
+          and until you do, it can be quite frustrating.
+
+          In retrospect, I'm actually glad I was so naive in the beginning. Because I knew so little back then, I was motivated to think critically about everything I learned later on.
+
+          #softwaredeveloper #frontenddeveloper #job
+          `,
+          image: "/assets/posts/things-before-programming.jpg",
+          createdAt: "Tue Oct 11 2022 07:34:34 GMT+0100 (West Africa Standard Time)",
+          comments: [
+               {
+                    _id: "s6y7ty2r23e2",
+                    author: {
+                         id: "h64yT2dsD",
+                         name: "Favour Adeyemi",
+                         position: "Frontend Developer Intern",
+                         image: "/assets/users/favour-adeyemi.jpg",
+                    },
+                    content: "Thanks for sharing, It's assuring knowing that I'm not alone in this.",
+                    createdAt: "Tue Oct 11 2022 14:40:34 GMT+0100 (West Africa Standard Time)",
+                    likes: ["h64yT2dsD", "dgv52u87dgcx", "12d999hj"]
+               }
+          ],
+          likes: ["12d999hj"]
+     },
 ];
