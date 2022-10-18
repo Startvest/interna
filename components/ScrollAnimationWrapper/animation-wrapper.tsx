a725577855bb5696387f4d0e9bd7af77e73f6cad
fortune710 <fortune0208@yahoo.com>
2022-10-18 11:55:13 +0100

    edited animation wrapper component

--- components/ScrollAnimationWrapper/animation-wrapper.tsx
+++ components/ScrollAnimationWrapper/animation-wrapper.tsx
@@ -5,26 +5,29 @@ type AnimationWrapperProps = {
     children: React.ReactNode,
     type?: 'horizontal-slide-in'|'fade-in', //Fade in is default
     performOnce?: boolean,
+    className?: string,
 }
 
-export const ScrollAnimationWrapper: React.FC<AnimationWrapperProps> = ({ children, type, performOnce }) => {
+export const ScrollAnimationWrapper: React.FC<AnimationWrapperProps> = ({ children, type, performOnce, className }) => {
     const ref = useRef(null);
     const whileInView = useInView(ref, { once: performOnce ? true : false });
 
     const fadeInAnimation: MotionStyle = {
         opacity: whileInView ? 1 : 0,
         translateY: whileInView ? 0 : "none",
-        transition: "all 0.5s ease-in 0.5s"
+        transition: "all 0.5s ease-in 0.5s",
     }
 
     const horizontalSlideInAnimation: MotionStyle = {
         transform: whileInView ? "none" : "translateX(-200px)",
         opacity: whileInView ? 1 : 0,
-        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s"
+        transition: "all 0.9s cubic-bezier(0.17, 0.55, 0.55, 1) 0.5s",
     }
+
     
     return(
         <motion.div 
+            className={className}
             style={type === "horizontal-slide-in" ? horizontalSlideInAnimation : fadeInAnimation}
             ref={ref}>
             {children}
