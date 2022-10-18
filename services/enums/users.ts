1683bdc725206f690abe25ad82c273c5d2858dc6
fortune710 <fortune0208@yahoo.com>
2022-10-18 11:55:23 +0100

    added test users file

---
+++ services/enums/users.ts
@@ -0,0 +1,32 @@
+export const users = [
+        {
+        id: "dgv52u87dgcx",
+        name: "Fortune Alebiosu",
+        position: "Intern at Microsoft",
+        image: "/assets/images/Fortune.jpg",
+        connected: false
+    },
+    {
+        id: "h64yT2dsD",
+        name: "Favour Adeyemi",
+        position: "Frontend Developer Intern",
+        image: "/assets/users/favour-adeyemi.jpg",
+        connected: true
+    },
+    {
+        id: "Fjd65Bs8pwq6",
+        name: "Elizabeth Akpan",
+        position: "Frontend Intern at O-Pay",
+        image: "/assets/users/elizabeth-akpan.jpg",
+        connected: false
+    },
+    {
+        id: "sd732gdg09F ",
+        name: "Mubarak Showole",
+        position: "Intern at uLesson",
+        image: "/assets/users/mubarak-showole.jpg",
+        connected: false
+
+    }
+
+] as const;
