0ca0a96671485a77be3f1a51db1786135d5d6e9a
fortune710 <fortune0208@yahoo.com>
2022-10-18 14:13:14 +0100

    changes

--- pages/search/index.tsx
+++ pages/search/index.tsx
@@ -6,22 +6,33 @@ import { NavBar } from "../../components/FloatingNavbar";
 import { AppHeader } from "../../components/header";
 import { Searchbar } from "../../components/Searchbar"
 
+import { users } from "../../services/enums/users";
+
 
 import styles from './search.module.scss';
 const SearchPage: React.FC = () => {
 
-    const [searchText, setSearchText] = useState('');
+    const [searchText, setSearchText] = useState({ text: '', searching: false });
     const [focused, setFocused] = useState(false);
     const [filters, setFilters] = useState(false);
+
+    const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+        if(!searchText.text){
+            setFocused(true);
+        } else {
+            setFocused(false);
+        }
+        setSearchText({ text: e.target.value, searching: true });
+    }
     return(
         <>
         <AppHeader pageName="Search for Anything"/>
         <Searchbar 
-                onBlur={() => setFocused(false)}
-                onFocus={() => setFocused(true)}
-                placeholder="Search for anything"
-                onChange={(e) => setSearchText(e.target.value)}
-            />
+            onBlur={() => setFocused(false)}
+            onFocus={() => setFocused(true)}
+            placeholder="Search for anything"
+            onChange={(e) =>handleSearch(e)}
+        />
         <main className={styles.container}>
             <h2>Search</h2>
             
@@ -87,7 +98,25 @@ const SearchPage: React.FC = () => {
                         <IoClose/>
                     </span>
                 </div>
+            </section>
 
+            <section className={`${!searchText.searching ? styles.invisible : null}`}>
+                {
+                    users.filter(user => user.name.toLowerCase().includes(searchText.text.toLowerCase())).map(user => (
+                        <div className="container space-between">
+                            <div className="flex gap">
+                                <Avatar src={user.image} size="small"/>
+                                <span>
+                                    <h3>{user.name}</h3>
+                                    <p>{user.position}</p>
+                                </span>
+                            </div>
+                            <Button className={user.connected ? styles.connected : ''}>
+                                {user.connected ? 'Connected' : 'Connect'}
+                            </Button>
+                        </div>
+                    ))
+                }
             </section>
             
             
