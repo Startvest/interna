5d6b5febaeb334a79e988b6183d7a4275c4a6963
fortune710 <fortune0208@yahoo.com>
2022-10-19 14:50:17 +0100

    created back button component

---
+++ components/BackButton/back-button.tsx
@@ -0,0 +1,14 @@
+import { useRouter } from "next/router";
+import { MdArrowBack } from "react-icons/md"
+
+export const BackButton: React.FC = () => {
+    const router = useRouter();
+
+    return(
+        <div className="flex items-center justify-start">
+            <span onClick={() => router.back()}>
+                <MdArrowBack size={25}/>
+            </span>
+        </div>
+    )
+}
