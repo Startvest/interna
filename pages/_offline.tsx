3e85afe00ecb64332da4b89237b565b590d68888
fortune710 <fortune0208@yahoo.com>
2022-10-29 21:46:43 +0100

    created offline and 404 pages

--- pages/_offline.tsx
+++ pages/_offline.tsx
@@ -1,10 +1,45 @@
 import type { NextPage } from 'next';
+import { useTheme } from 'next-themes';
+import Image from 'next/image';
+import { Button } from '../components/Button';
+import { Header } from '../components/header';
+
+import styles from '../styles/offline.module.scss';
 
 const Offline: NextPage = () => {
+  const { resolvedTheme } = useTheme();
   return (
-    <div>
-      <h1>You are offline</h1>
-    </div>
+    <>
+      <Header pageName='You are Offline'/>
+      <main className={styles.mainContent}>
+        {
+          resolvedTheme == 'light' ?
+          <Image 
+            src="/assets/no-signal.svg" 
+            width={'256px'} height={'256px'}
+            layout={'fixed'} 
+            alt="no signal" title="no signal"
+          />
+          : resolvedTheme == 'dark' ?
+          <Image 
+            src="/assets/no-signal-dark.svg" 
+            width={'256px'} height={'256px'}
+            layout={'fixed'} 
+            alt="no signal" title="no signal"
+          />
+          : null
+        }
+
+        <h3>Seems you're offline</h3>
+
+        <p>Please check your internet connection and refresh this page</p>
+
+        <Button className={`${styles.mainButton} flex items-center justify-center`}>
+          Back to Homepage
+        </Button>
+      </main>
+      
+    </>
   );
 };
 export default Offline;
