350d5782f6fccfa5bdf6308e3ff711ebc7ddcd9e
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-10-30 19:52:05 +0100

    navbar created

---
+++ components/Admin/navbar.tsx
@@ -0,0 +1,49 @@
+import styles from './admin.module.scss';
+import {useRouter} from 'next/router';
+import {useEffect} from 'react';
+import Image from "next/image";
+import Link from 'next/link';
+import { useTheme } from 'next-themes';
+import { 
+     IoTrendingUpOutline,
+     IoFlagOutline,
+     IoFlag,
+     IoDocumentText,
+     IoDocumentTextOutline,
+     IoSettingsOutline,
+     IoLogOutOutline
+} from 'react-icons/io5';
+
+type pathNames = '/admin/dashboard' | '/admin/moderation' | '/admin/logs' | '/admin/settings';
+
+
+export function NavigationBar(){
+     const router = useRouter();
+     const path:pathNames = router.pathname as pathNames
+     const { setTheme } = useTheme();
+     useEffect(() =>{
+          setTheme('light');
+     },[])
+     
+     let dashIcon= path === '/admin/dashboard' ? <IoTrendingUpOutline size={16} className={`${styles.navItemsLogo} ${styles.navItemsLogoActive}`}/> : <IoTrendingUpOutline size={16} className={styles.navItemsLogo}/>;
+     let flagIcon= path === '/admin/moderation' ? <IoFlag size={16} className={styles.navItemsLogo}/> : <IoFlagOutline size={16} className={styles.navItemsLogo}/>
+     let docsIcon= path === '/admin/logs' ? <IoDocumentText size={16} className={styles.navItemsLogo}/> : <IoDocumentTextOutline size={16} className={styles.navItemsLogo}/>
+     let settingsIcon= path === '/admin/settings' ? <IoSettingsOutline size={16} className={styles.navItemsLogo}/> : <IoSettingsOutline size={16} className={styles.navItemsLogo}/>
+
+     return(
+          <div className={styles.navbar}>
+               <Image src="/assets/logo-white.svg" className={styles.logo} height={'50px'} width={'222px'}/>
+               <h3>Admin Panel</h3>
+               <div className={styles.navItems}>
+                    <Link href={'/admin/dashboard'}><span>{dashIcon} User Analytics</span></Link>
+                    <Link href={'/admin/moderation'}><span>{flagIcon} Moderation</span></Link>
+                    <Link href={'/admin/logs'}><span>{docsIcon} Logs</span></Link>
+                    <Link href={'/admin/settings'}><span>{settingsIcon} Settings</span></Link>
+               </div>
+
+               <div className={styles.logOut}>
+                    <IoLogOutOutline size={20} className={styles.navItemsLogo}/> Log out
+               </div>
+          </div>
+     )
+}
---
+++ pages/admin/dashboard.tsx
@@ -0,0 +1,14 @@
+import {NavigationBar} from '../../components/Admin';
+import {Header} from '../../components/header';
+
+const Dashboard = () => {
+     return(
+          <>
+               <Header pageName='Admin Dashboard | Interna'/>
+               <NavigationBar/>
+          </>
+     )
+
+}
+
+export default Dashboard;
