f0bbe70c8cb52f57854e06c701bda2f79d073c28
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-11-09 22:24:35 +0100

    ui bug fixed, tools text added

---
+++ services/enums/tools.ts
@@ -0,0 +1,26 @@
+export const tools = [
+     {
+          icon: "",
+          text: "CV Generator"
+     },
+     {
+          icon: "",
+          text: "Interview Prep"
+     },
+     {
+          icon: "",
+          text: "Secure Private Messaging"
+     },
+     {
+          icon: "",
+          text: "Remote Jobs"
+     },
+     {
+          icon: "",
+          text: "Mentorship sessions"
+     },
+     {
+          icon: "",
+          text: "Jobs from partner firms"
+     },
+] as const
