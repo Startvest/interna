493b1a755fc8d84dff0d662198d01beda8d132df
fortune710 <fortune0208@yahoo.com>
2022-11-14 10:16:05 +0100

    exported suggestions api call

--- services/waitlist/index.ts
+++ services/waitlist/index.ts
@@ -2,6 +2,7 @@ import { Interface } from 'readline';
 import { axios } from '../../config/axios';
 import {IUser} from '../../pages/api/waitlist';
 
+export * from './suggestions';
 
 export async function addUser(data: Partial<IUser>) {
   const res = await axios.post<IUser>('/waitlist', data, { headers: {"Authorization" : `${process.env.NEXT_PUBLIC_AUTH}`} });
