f3862da7bdbda8dd8f309e938727f033cae1bff1
Hanif adedotun <hanifadedotun2k19@gmail.com>
2022-11-30 14:57:06 +0100

    fixed merge conflict

--- components/SideMenu/menu.tsx
+++ components/SideMenu/menu.tsx
@@ -29,7 +29,6 @@ export const SideMenu: React.FC<SideMenuProps> = ({ isOpen, hasBeenDismissed }) 
     return(
         <div className={styles.backdrop}>
             <section>
-
                 <div className={styles.profile}>
                     <Avatar src="/assets/images/user2.svg" />
                     <h2>John Doe</h2>
