cf8d581a01944d95d6178fbb13ec0c5eb71e5cae
fortune710 <fortune0208@yahoo.com>
2022-12-29 18:06:05 +0100

    created tag input component

---
+++ components/Input/tags-input.tsx
@@ -0,0 +1,23 @@
+import { TagsInput } from 'react-tag-input-component';
+import styles from './input-styles.module.scss';
+
+interface TagInputProps {
+    name: string,
+    labelName: string,
+    className?: string,
+    value: string[],
+    onChange?: (value: string[]) => void,
+}
+export const TagInput: React.FC<TagInputProps> = ({ name, labelName, className, value, onChange }) => {
+    return(
+        <div className={`${styles.inputGroup} ${className}`}>
+            <label htmlFor={name} className={styles.inputLabel}>
+                {labelName}
+            </label>
+            <TagsInput
+                value={value}
+                onChange={onChange}
+            />
+        </div>
+    )
+}
