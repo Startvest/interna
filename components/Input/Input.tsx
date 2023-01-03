9157334482ff3d06e71c530424f8c8d4f24deddb
fortune710 <fortune0208@yahoo.com>
2023-01-03 13:37:32 +0100

    modified input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -9,21 +9,22 @@ type InputProps = {
     onChange?: any,
     className?: string,
     value?: string|number|string[],
-    reg?: any,
-    error?: string,
     useTags?: boolean,
+    isDisabled?:boolean
+    reg?:any,
+    error?: string
 }
 
 
 export const Input:React.FC<InputProps> = (props) => {
-    const { className, useTags, value, type, name, onChange, inputClassName, placeholder, labelName, reg, error} = props;
+    const { isDisabled, className, useTags, value, type, name, onChange, inputClassName, placeholder, labelName, reg, error} = props;
     
     return(
         <div className={`${styles.inputGroup} ${className}`}>
             <label htmlFor={name} className={styles.inputLabel}>
                 {labelName}
             </label>
-            <input value={value} onChange={() => onChange} className={inputClassName} id={name} type={type} title={name} placeholder={placeholder}  {...reg}/>
+            <input disabled={isDisabled} value={value} onChange={() => onChange} className={inputClassName} id={name} type={type} title={name} placeholder={placeholder}  {...reg}/>
             {error && <span className={styles.errorText}>{error}</span>}
         </div>
     )
--- components/Input/index.tsx
+++ components/Input/index.tsx
@@ -1,3 +1,4 @@
 export * from './Input';
 export * from './select';
-export * from './tags-input'
+export * from './tags-input';
+export  * from './textarea';
