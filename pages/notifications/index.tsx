4f7e74bde19586aa6feabccf8dc8334275937b42
fortune710 <fortune0208@yahoo.com>
2023-01-04 18:30:35 +0100

    changes

--- pages/notifications/index.tsx
+++ pages/notifications/index.tsx
@@ -1,38 +1,43 @@
+import { useState } from "react";
 import { NavBar } from "../../components/FloatingNavbar";
 import { AppHeader } from "../../components/header";
 import { Notification } from "../../components/Notification";
 import styles from "../../components/Notification/notification.module.scss";
 
 const NotificationsPage: React.FC = () => {
+    const notification = {
+        title: "You just gained a new follower!",
+        content: "Fortune Alebiosu started following you.",
+        dateCreated: "Tue, 11 Oct 2022 15:43:35 GMT"
+    }
+
+    const [notifications, setNotifications] = useState([
+        notification, notification, notification
+    ])
+
     return(
     <main>
         <AppHeader pageName={'Your Notifications'}/>
         <div className="content">
             <h2 className={styles.headText}>Notifications for you</h2>
-
-            <Notification
-                title="You just gained a new follower!"
-                content="Fortune Alebiosu started following you."
-                dateCreated={'Tue, 11 Oct 2022 15:43:35 GMT'}
-            />
-
-            <Notification
-                title="You just gained a new follower 2!"
-                content="Fortune Alebiosu started following you."
-                dateCreated={'Tue, 11 Oct 2022 16:43:35 GMT'}
-            />
-            
-            <Notification
-                title="You just gained a new follower 3!"
-                content="Fortune Alebiosu started following you."
-                dateCreated={'Tue, 11 Oct 2022 12:43:35 GMT'}
-            />
-
+            {
+                notifications.length === 0 ? <EmptyNotifications/>
+                : notifications.map((notification) => (
+                    <Notification {...notification}/>
+                ))
+            }            
         </div>
         <NavBar/>
 
     </main>
     )
 }
+
+const EmptyNotifications: React.FC = () => (
+    <div className={styles.emptyNotification}>
+        <img src="/assets/notification.svg" alt="empty-notifications"/>
+        <h3>Oops, looks like you are a silent mover....</h3>
+    </div>
+)
 
 export default NotificationsPage;
