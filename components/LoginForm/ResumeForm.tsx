a2b556620ca68b853a693c67ab5ca5bad0c9c9ef
Hanif adedotun <hanifadedotun2k19@gmail.com>
2023-01-10 15:12:21 +0100

    renamed IResume with IEXPerience

--- components/LoginForm/ResumeForm.tsx
+++ components/LoginForm/ResumeForm.tsx
@@ -1,138 +1,159 @@
-import styles from './login.module.scss';
-import {Input, Select} from '../../components/Input';
 import { useEffect, useState } from 'react';
-import { UseFormRegister, UseFormSetValue, useForm } from 'react-hook-form';
-import { Button } from '../Button';
-import { ICreateProfile, IResume } from '../../server/db';
+import { useForm } from 'react-hook-form';
+import { Input, Select } from '../../components/Input';
+import { IExperience } from '../../server/db';
+import { getSuggestions } from '../../services/waitlist';
 import { AutocompleteName } from '../AutocompleteName/autocomplete';
-import { getSuggestions } from "../../services/waitlist";
+import { Button } from '../Button';
+import styles from './login.module.scss';
 interface ResumeFormProps {
-    formRegister: any,//UseFormRegister<ICreateProfile>,
-    setFormValue: any,//UseFormSetValue<ICreateProfile>,
-    handleInputSave: Function
+  formRegister: any; //UseFormRegister<ICreateProfile>,
+  setFormValue: any; //UseFormSetValue<ICreateProfile>,
+  handleInputSave: Function;
 }
 
-type Postion = 'intern'|'student';
+type Postion = 'intern' | 'student';
 
-export const ResumeForm: React.FC<ResumeFormProps> = ({ setFormValue, formRegister, handleInputSave }) =>{
-    const [suggestions, setSuggestions] = useState<any[]|undefined>(undefined);
-    const [checked, setChecked] = useState<boolean>(false);
-    const [experiences, setExperiences] = useState<IResume[]>([]);
-    const [query, setQueryText] = useState<string>('');
+export const ResumeForm: React.FC<ResumeFormProps> = ({
+  setFormValue,
+  formRegister,
+  handleInputSave,
+}) => {
+  const [suggestions, setSuggestions] = useState<any[] | undefined>(undefined);
+  const [checked, setChecked] = useState<boolean>(false);
+  const [experiences, setExperiences] = useState<IExperience[]>([]);
+  const [query, setQueryText] = useState<string>('');
 
-    const { register, getValues, reset, setValue, formState } = useForm<IResume>({
-        defaultValues: {
-            company_name: "",
-            type: "intern",
-            start: "",
-            end: "",
-            current: checked,
-            logo: "assets/illustrations/job.svg"
-        }
-    })
+  const { register, getValues, reset, setValue, formState } =
+    useForm<IExperience>({
+      defaultValues: {
+        company_name: '',
+        type: 'intern',
+        start: '',
+        end: '',
+        current: checked,
+        logo: 'assets/illustrations/job.svg',
+      },
+    });
 
-    const addWorkToResume = () => {
-        const workExperience = getValues();
-        checked ? workExperience.end = 'present' : workExperience.end;
-        const { company_name, start, end } = workExperience;
-        
-        if(!company_name || !start || !end) return;
+  const addWorkToResume = () => {
+    const workExperience = getValues();
+    checked ? (workExperience.end = 'present') : workExperience.end;
+    const { company_name, start, end } = workExperience;
 
-        setExperiences((prev) => ([...prev, workExperience]));
-        reset()
-        setChecked(false)
-    }
+    if (!company_name || !start || !end) return;
 
-    useEffect(() => {
-        setFormValue('position', experiences)
-    }, [experiences])
+    setExperiences((prev) => [...prev, workExperience]);
+    reset();
+    setChecked(false);
+  };
 
-    useEffect(() => {
-        getSuggestions(query)
-        .then((res:any[]) => {
-            if(!res || res.length === 0) setValue('company_name', query);
-            const names = res?.map(item => { return item.name })
-            if(names?.includes(query)) return setSuggestions(undefined)
-            setSuggestions(res);
-        })
-    }, [query])
-    
-    return(
-          <section className={styles.formContainer}>
-            {
-                experiences && (
-                    experiences.map((val, key) => (<span key={key}>
-                        <h3 >{val.company_name}</h3>
-                        <h3 >{val.logo}</h3>
-                        </span>
-                    ))
-                )
-            }
-               <h2>Resume</h2>
-               
-               <Input
-                    type="text"
-                    name="company_name"
-                    onChange={(e: any) => {handleInputSave(); setQueryText(e.target.value)}}
-                    placeholder="Enter the company's name"
-                    labelName={'Company Name'}
-                    className={styles.input}
-                    reg={register('company_name', {required: true})}
-               />
-               <AutocompleteName 
-                    suggestions={suggestions} 
-                    setSelected={(e:any, companyName:string, logo:string) => {
-                        setSuggestions(undefined);
-                        setValue('company_name', companyName);
-                        setValue('logo', logo);
-                        setQueryText(companyName); //Changes the Value of the input field as well
-                    }}/>
+  useEffect(() => {
+    setFormValue('position', experiences);
+  }, [experiences]);
 
-               <Select
-                    name="position"
-                    onChange={(e) => {
-                        const value = e.target.value as IResume["type"];
-                        setValue('type', value)
-                        handleInputSave();
-                    }}
-                    placeholder="Were you a student or an Intern"
-                    labelName={'Postion'}
-                    className={styles.input}
-                >
-                    <option title='Intern' value="intern">Intern</option>
-                    <option title="Student" value="student">Student</option>
-                </Select>
+  useEffect(() => {
+    getSuggestions(query).then((res: any[]) => {
+      if (!res || res.length === 0) setValue('company_name', query);
+      const names = res?.map((item) => {
+        return item.name;
+      });
+      if (names?.includes(query)) return setSuggestions(undefined);
+      setSuggestions(res);
+    });
+  }, [query]);
 
-                <div className="flex gap justify-between">
-                    <Input
-                        type="date"
-                        name="start_date"
-                        onChange={(e: any) => handleInputSave()}
-                        // placeholder="www.portfolio.com"
-                        labelName={'Start Date'}
-                        className={styles.smallInput}
-                        reg={register('start', {required: true})}
-                    />
+  return (
+    <section className={styles.formContainer}>
+      {experiences &&
+        experiences.map((val, key) => (
+          <span key={key}>
+            <h3>{val.company_name}</h3>
+            <h3>{val.logo}</h3>
+          </span>
+        ))}
+      <h2>Resume</h2>
 
-                    <Input
-                        type="date"
-                        name="end_date"
-                        onChange={(e: any) => handleInputSave()}
-                        // placeholder="www.portfolio.com"
-                        labelName={'End Date'}
-                        className={styles.smallInput}
-                        isDisabled={checked}
-                        reg={register('end', {required: true})}
-                    />
+      <Input
+        type="text"
+        name="company_name"
+        onChange={(e: any) => {
+          handleInputSave();
+          setQueryText(e.target.value);
+        }}
+        placeholder="Enter the company's name"
+        labelName={'Company Name'}
+        className={styles.input}
+        reg={register('company_name', { required: true })}
+      />
+      <AutocompleteName
+        suggestions={suggestions}
+        setSelected={(e: any, companyName: string, logo: string) => {
+          setSuggestions(undefined);
+          setValue('company_name', companyName);
+          setValue('logo', logo);
+          setQueryText(companyName); //Changes the Value of the input field as well
+        }}
+      />
 
-                </div>
-                <div className={`${styles.present} flex items-center justify-start`}>
-                    <input checked={checked} onChange={(e) => setChecked(e.target.checked)} type="checkbox" name="present" id="present"/>
-                    <label htmlFor="present">I currently work here</label>
-                </div>
-                <Button type='button' onClick={addWorkToResume} className={styles.saveBtn}>
-                    Save work experience
-                </Button>
-          </section>
-     )
-}
+      <Select
+        name="position"
+        onChange={(e) => {
+          const value = e.target.value as IExperience['type'];
+          setValue('type', value);
+          handleInputSave();
+        }}
+        placeholder="Were you a student or an Intern"
+        labelName={'Postion'}
+        className={styles.input}
+      >
+        <option title="Intern" value="intern">
+          Intern
+        </option>
+        <option title="Student" value="student">
+          Student
+        </option>
+      </Select>
+
+      <div className="flex gap justify-between">
+        <Input
+          type="date"
+          name="start_date"
+          onChange={(e: any) => handleInputSave()}
+          // placeholder="www.portfolio.com"
+          labelName={'Start Date'}
+          className={styles.smallInput}
+          reg={register('start', { required: true })}
+        />
+
+        <Input
+          type="date"
+          name="end_date"
+          onChange={(e: any) => handleInputSave()}
+          // placeholder="www.portfolio.com"
+          labelName={'End Date'}
+          className={styles.smallInput}
+          isDisabled={checked}
+          reg={register('end', { required: true })}
+        />
+      </div>
+      <div className={`${styles.present} flex items-center justify-start`}>
+        <input
+          checked={checked}
+          onChange={(e) => setChecked(e.target.checked)}
+          type="checkbox"
+          name="present"
+          id="present"
+        />
+        <label htmlFor="present">I currently work here</label>
+      </div>
+      <Button
+        type="button"
+        onClick={addWorkToResume}
+        className={styles.saveBtn}
+      >
+        Save work experience
+      </Button>
+    </section>
+  );
+};
