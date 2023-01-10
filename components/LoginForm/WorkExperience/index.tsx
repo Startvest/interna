6a8f8655c7138b087db3533c8b31c1b34d13ad6a
Hanif adedotun <hanifadedotun2k19@gmail.com>
2023-01-10 17:05:23 +0100

    experience component added

---
+++ components/LoginForm/WorkExperience/index.tsx
@@ -0,0 +1,10 @@
+import { IExperience } from "../../../server/db"
+
+
+export const WorkExperience = ({experience}:{experience:IExperience}) => {
+     return (
+          <div>
+               
+          </div>
+     )
+}
